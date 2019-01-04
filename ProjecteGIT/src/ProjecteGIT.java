b942201b4af1e67a117b9f378dc460fa30faa5fe
zerosfinal <zerosfinal@zerosfinal>
2019-01-04 18:19:08 +0100

    Agrego scanner

--- ProjecteGIT/src/ProjecteGIT.java
+++ ProjecteGIT/src/ProjecteGIT.java
@@ -1,9 +1,15 @@
 
+import java.util.Scanner;
+
 public class ProjecteGIT {
 
-	public static void main(String[] args) {
-		System.out.println("Disme un numero");
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Disme un numero");    
+        int a = entrada.nextInt();
 
-	}
+    }
 
 }
+
