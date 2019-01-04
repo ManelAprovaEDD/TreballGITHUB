bfba671ea28a7e060f3e8f5f0084fe810249d3b1
zerosfinal <zerosfinal@zerosfinal>
2019-01-04 19:41:57 +0100

    TAG: programa final

--- ProjecteGIT/src/ProjecteGIT.java
+++ ProjecteGIT/src/ProjecteGIT.java
@@ -6,23 +6,27 @@ public class ProjecteGIT {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("Disme un numero");    
+        System.out.print("Disme un numero: ");    
         int a = entrada.nextInt();
-        System.out.println("Disme un altre numero"); 
+        System.out.print("Disme un altre numero: "); 
         int b = entrada.nextInt();
         
         if(a>b){
 
         	System.out.println("El numero " + a + " es major que el numero " + b);
         }
+        if (a == b) {
+        	System.out.println("El numero " + a + " es igual que el numero " + b );
+        
+        }
         
         if(b>a){
         	System.out.println("El numero " + b + " es major que el numero " + a);
         }
     
-        System.out.println("El numero " + a + " mas el numero " + b + " es igual a " + (a+b));
+        System.out.println("El numero " + a + " mes el numero " + b + " es igual a " + (a+b));
         
-        System.out.println("El numero " + a + " menos el numero " + b + " es igual  a " + (a-b));
+        System.out.println("El numero " + a + " menos el numero " + b + " es igual a " + (a-b));
         
 
     }
