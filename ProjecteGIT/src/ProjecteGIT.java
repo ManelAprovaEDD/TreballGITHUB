6e4df777426aef9482eb5bbc605a35b46b5e70cd
Roda <Roda@RODAPC.home>
2019-01-04 18:52:32 +0100

    if a > b

--- ProjecteGIT/src/ProjecteGIT.java
+++ ProjecteGIT/src/ProjecteGIT.java
@@ -10,6 +10,10 @@ public class ProjecteGIT {
         int a = entrada.nextInt();
         System.out.println("Disme un altre numero"); 
         int b = entrada.nextInt();
+        
+        if(a>b){
+        	System.out.println("El numero" + a + "es major que el numero" + b);
+        }
     }
 
 }
